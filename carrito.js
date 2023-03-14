094f6708b784bc55785274894012bd257d246651
lauramorales <ma.lauramorales@gmail.com>
2023-03-14 13:22:59 -0300

    4

--- carrito.js
+++ carrito.js
@@ -1,5 +1,4 @@
-
-function welcome(){
+function welcome() {
     let saludar = 'Welcome to your original SNKRS';
     alert('Welcome to your original SNKRS');
 }
@@ -8,15 +7,15 @@ welcome();
 
 let question = prompt('Deseas seleccionar genero del producto? \n(y-yes / n-no)');
 
-while (question == 'y'){
+while (question == 'y') {
     let answer = prompt('1-Women\n2-Men\n3-Kids');
-    if (answer == '1'){
+    if (answer == '1') {
         alert('Elegiste genero Women');
-    }else if(answer == '2'){
+    } else if (answer == '2') {
         alert('Elegiste genero Men');
-    }else if(answer == '3'){
+    } else if (answer == '3') {
         alert('Elegiste genero Kids')
-    }else{
+    } else {
         alert('Genero inexistente');
     }
     question = prompt('Deseas cambiar el genero? (y-yes / n-no)');
@@ -24,27 +23,27 @@ while (question == 'y'){
 
 let sneakers = prompt('Elegi el modelo y sabras el precio:\n \n1 - Graffitti\n2 - Flor\n3 - Lips\n4 - Osiris\n\nB / para volver al menu');
 
-while(sneakers != 'B'){
-switch(sneakers){
-    case '1':
-        alert('Tu modelo elegido cuesta $160');
-        break;
-    case '2':
-        alert('Tu modelo elegido cuesta $140');
-        break;
-    case '3':
-        alert('Tu modelo elegido cuesta $110');
-        break;
-    case '4':
-        alert('Tu modelo elegido cuesta $200');
-        break;
+while (sneakers != 'B') {
+    switch (sneakers) {
+        case '1':
+            alert('Tu modelo elegido cuesta $160');
+            break;
+        case '2':
+            alert('Tu modelo elegido cuesta $140');
+            break;
+        case '3':
+            alert('Tu modelo elegido cuesta $110');
+            break;
+        case '4':
+            alert('Tu modelo elegido cuesta $200');
+            break;
 
-    default:
-        alert('Modelo Inexistente');
-        break;
+        default:
+            alert('Modelo Inexistente');
+            break;
 
-}
-sneakers = prompt('Ingresa el modelo y sabras el precio:\n \n1 - Graffitti\n2 - Flor\n3 - Lips\n4 - Osiris\n\nB / para volver al menu');
+    }
+    sneakers = prompt('Ingresa el modelo y sabras el precio:\n \n1 - Graffitti\n2 - Flor\n3 - Lips\n4 - Osiris\n\nB / para volver al menu');
 }
 
 
@@ -54,39 +53,12 @@ let flor = 160;
 let lips = 140;
 let osiris = 190;
 
-let compra = graffitti+flor+lips+osiris;
+let compra = graffitti + flor + lips + osiris;
 
 
 function sumatoriaCompra(totalSneakers) {
     let total = totalSneakers;
-    alert('El total de su compra es $'+total);
-} 
+    alert('El total de su compra es $' + total);
+}
 
 sumatoriaCompra(compra);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
